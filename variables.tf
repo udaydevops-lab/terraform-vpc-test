67718dc360a988fb1d22d0b6b7832b198b7e98e0
udaydevops-lab <darauday0@gmail.com>
2024-12-17 20:34:37 +0530

    devops practice

--- variables.tf
+++ variables.tf
@@ -9,3 +9,16 @@ variable "common_tags" {
         Terraform = "true"
     }
 }
+
+
+variable "public_subnet_cidrs" {
+    default = ["10.0.1.0/24","10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+    default = ["10.0.11.0/24","10.0.12.0/24"]
+}
+
+variable "database_subnet_cidrs" {
+    default = ["10.0.21.0/24","10.0.22.0/24"]
+}
--- vpc.tf
+++ vpc.tf
@@ -3,8 +3,8 @@ module "vpc" {
     #source = "git::https://github.com/daws-78s/terraform-aws-vpc.git?ref=main"
     project_name = var.project_name
     common_tags = var.common_tags
-    # public_subnet_cidrs = var.public_subnet_cidrs
-    # private_subnet_cidrs = var.private_subnet_cidrs
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
     # database_subnet_cidrs = var.database_subnet_cidrs
     # is_peering_required = var.is_peering_required
 }
